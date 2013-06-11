4dfeed42be823c068296d8797c009dd7fb498cca
Ari Lerner <arilerner@mac.com>
2013-06-11 02:52:18 -0700

    Updated email

--- bin/app.rb
+++ bin/app.rb
@@ -2,7 +2,7 @@ require 'json'
 
 class App < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), "..")
-  set :public, settings.root
+  set :public_dir, settings.root
   
   get '/' do
     File.read(File.expand_path('index.html', settings.root))
