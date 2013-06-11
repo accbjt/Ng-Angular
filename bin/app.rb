7a8fad600113c67f48fab90e2ae4022a44359d8a
Ari Lerner <arilerner@mac.com>
2013-06-11 02:44:28 -0700

    Initial checkin

---
+++ bin/app.rb
@@ -0,0 +1,14 @@
+require 'json'
+
+class App < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), "..")
+  set :public, settings.root
+  
+  get '/' do
+    File.read(File.expand_path('index.html', settings.root))
+  end
+  
+  get '/api/:p' do
+    File.read(File.expand_path('js/data/' + params['p'] + '.json', settings.root))
+  end
+end
